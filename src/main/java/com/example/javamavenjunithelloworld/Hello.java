8266bad9ad26271d7ce9dfd8c39c043215bbb389
maheshgradious <117516986+maheshgradious@users.noreply.github.com>
2023-05-03 15:49:30 +0530

    Update Hello.java

--- src/main/java/com/example/javamavenjunithelloworld/Hello.java
+++ src/main/java/com/example/javamavenjunithelloworld/Hello.java
@@ -38,6 +38,25 @@ public class Hello {
         for (short i = 0; i < times; i++) {
             printer.println(HELLO);
         }
+        public void setTimes(int times) {
+        if (times < 0 || times > MAXIMUM_AMOUNT_OF_TIMES) {
+            throw new IllegalArgumentException(String.format(
+                    "Parameter «times» should be a positive number no larger than %d.",
+                    MAXIMUM_AMOUNT_OF_TIMES
+            ));
+        }
+        this.times = times;
+    }
+
+    /**
+     * Say "Hello!".
+     *
+     * @param printer PrintStream to write output to.
+     */
+    public void sayHello(PrintStream printer) {
+        for (short i = 0; i < times; i++) {
+            printer.println(HELLO);
+        }
     }
 
 }
